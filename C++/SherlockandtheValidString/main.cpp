5f366ccbe9bd57cfa75dc2726b749933f5789c41
polar zhang <zhyuanshan@icloud.com>
2021-01-16 12:04:01 +0800

    Optimize code.

--- C++/SherlockandtheValidString/main.cpp
+++ C++/SherlockandtheValidString/main.cpp
@@ -1,42 +1,41 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 // Complete the isValid function below.
 string isValid(string s) {
     map<char, int> m;
-    for (auto c: s) {
+    for (auto c : s) {
         m[c] ++;
     }
 
     map<int, int> countMap;// it->first: occurency, it->second: letter count.
-    for (auto it = m.begin(); it != m.end(); it++) {
-        cout << it->first << "-" << it->second << " ";
-        auto countIt = countMap.find(it->second);
-        if (countIt == countMap.end()) {
-            countMap[it->second] = 1;
-            if (countMap.size() > 2) {
-                return "NO";
-            }
-        }
-        else {
-            countIt->second++;
+    for (auto p : m) {
+        countMap[p.second] ++;
+        if (countMap.size() > 2) {
+            return "NO"; // more than two differences
         }
     }
 
-    int size = countMap.size();
-    auto it = countMap.begin();
-    auto it2 = it++;
-    if (size == 1 || // same occurent
-        size == 2 && (it->first == 1 && it->second == 1 || it2->first == 1 && it2->second == 1 || // only letter and appear only onice
-            it->second == 1 && it->first - it2->first == 1 || it2->second == 1 && it2->first - it->first == 1)) // only letter and appear once more than others
+    auto it = countMap.begin(); // first element 
+    auto it2 = it;
+    advance(it2, 1); // second element
+
+    cout << "p1: " << it->first << "-" << it->second << " p2: " << it2->first << "-" << it2->second << endl;
+
+    if (countMap.size() != 0 &&
+        (countMap.size() == 1 || // same occurent for all letters
+        it->first == 1 && it->second == 1 || // only letter and appear only once
+        it2->second == 1 && it2->first - it->first == 1)) // only letter and appear once more than others
     {
         return "YES";
     }
 
-    return "NO";
+    return "NO"; // probably size == 0
 }
 
 int main()
