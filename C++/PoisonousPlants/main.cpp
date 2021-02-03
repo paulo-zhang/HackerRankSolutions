29b9219622e815427ea214e736c6eccb693c51b8
polar zhang <zhyuanshan@icloud.com>
2021-02-03 12:08:11 +0800

    Borrowed algorithm.

--- C++/PoisonousPlants/main.cpp
+++ C++/PoisonousPlants/main.cpp
@@ -3,33 +3,75 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <stack>
+#include <deque>
 
 using namespace std;
 
 vector<string> split_string(string);
 
+//https://www.hackerrank.com/challenges/poisonous-plants/forum
+
+//w00tles(https://www.hackerrank.com/w00tles)
+//it can be done with a list of stacks *
+//
+//let's say we start with
+//
+//6 5 8 7 4 7 3 1 1 10
+//
+//we'll represent it as a list of stacks like this:
+//
+//monday 6 > 5, 8 > 7 > 4, 7 > 3 > 1 > 1, 10
+//
+//each stack is a maximal non - increasing chain
+//
+//now, to let one day pass :
+//
+//pop the top element from every stack(excluding the unique leftmost stack).these are the plants that die on this day.
+//tuesday 6 > 5, 7 > 4, 3 > 1 > 1, -
+//
+//now check to see if any adjacent stacks can be merged.here we see that 7 > 4 and 3 > 1 > 1 can be merged to form a longer chain.stacks can be merged in O(1)
+//tuesday 6 > 5, 7 > 4 > 3 > 1 > 1
+//
+//one day has now passed.repeat the process.
+//wednesday 6 > 5, 4 > 3 > 1 > 1
+//
+//wednesday 6 > 5 > 4 > 3 > 1 > 1
+//
+//now that just one stack remains in our list, we know we are done.
+//NB these stacks are linked - list - based and maintain a pointer to their base element.This allows for O(1) merge operations, and O(1) checking of the topand bottom values.
+
 // Complete the poisonousPlants function below.
 int poisonousPlants(vector<int> p) {
-    int days = 0;
+    vector<int> days(p.size());
+    int minPoison = p[0];
+    int maxDay = 0;
+    stack<int> traversedIndexs;
 
-    while (p.size() > 1) {
-        int size = p.size();
-        for (int i = size - 1; i > 0; i--) {
-            if (p[i] > p[i - 1]) {
-                // dies
-                p.erase(p.begin() + i);
+    traversedIndexs.push(0);
+
+    for (int i = 1; i < p.size(); i++)
+    {
+        if (p[i] > p[i - 1]) {
+            days[i] = 1; // Bigger than previous number, die on the first day.
+        }
+
+        minPoison = min(minPoison, p[i]);// Mark mallest.
+
+        while (!traversedIndexs.empty() && p[traversedIndexs.top()] >= p[i]) // Smaller than previous number (belong to current stack) or the prevoius stack top (merge with previous stack).
+        {
+            if (p[i] > minPoison) {
+                days[i] = max(days[i], days[traversedIndexs.top()] + 1); // die after previous number (orignal stack) or the number of previous stack (merge stack scenario).
             }
+
+            traversedIndexs.pop(); // Remove trace, since this number won't be relevant to the numbers behind.
         }
 
-        if (size == p.size()) {
-            // no plant dies.
-            break;
-        }
-
-        days++;
+        maxDay = max(maxDay, days[i]); // Is this the max?
+        traversedIndexs.push(i); // Mark trace for future number.
     }
 
-    return days;
+    return maxDay;
 }
 
 int main()
