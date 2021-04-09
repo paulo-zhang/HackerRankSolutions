b5c4402255a8ae18a34c489127acb71b28e6ae36
polar.zhang <polar@xsintlg.com>
2021-04-09 17:50:42 +0800

    Optimize.

--- C++/ReverseShuffleMerge/main.cpp
+++ C++/ReverseShuffleMerge/main.cpp
@@ -9,58 +9,58 @@ using namespace std;
 // Implement base on https://www.hackerrank.com/challenges/reverse-shuffle-merge/forum/comments/77705
 // Complete the reverseShuffleMerge function below.
 string reverseShuffleMerge(string s) {
-    map<char, int> counts;
+    int counts[26] = { 0 }, passedCounts[26] = { 0 }, AddedCounts[26] = { 0 };
     string result;
 
     for (char c : s) {
-        counts[c] ++;
+        counts[c - 'a'] ++;
     }
 
-    for (auto it : counts) {
-        counts[it.first] = it.second / 2;
+    for (int i = 0; i < 26; i++) {
+        counts[i] = counts[i] / 2;
     }
 
     int lastIndex = s.size();
-    map<char, int> passedCounts, AddedCounts;
     for (int i = s.size() - 1; i >= 0 && result.size() < s.size() / 2; i--) {
         char c = s[i];
+        int index = c - 'a';
 
-        passedCounts[c] ++;
-        if (counts[c] == AddedCounts[c]) {
+        passedCounts[index] ++;
+        if (counts[index] == AddedCounts[index]) {
             continue;// Ignore
         }
 
-        if (counts[c] < passedCounts[c] - AddedCounts[c]) {// Cannot afford to miss this one.
+        if (counts[index] < passedCounts[index] - AddedCounts[index]) {// Cannot afford to miss this one.
             char smallest;
             do {
                 smallest = 'z' + 1;
                 // Find the smallest wanted character
                 int n = i;
                 for (int j = lastIndex - 1; j >= i; j--) {
-                    if (s[j] < smallest && AddedCounts[s[j]] < counts[s[j]]) {
+                    if (s[j] < smallest && AddedCounts[s[j] - 'a'] < counts[s[j] - 'a']) {
                         smallest = s[j];
                         n = j;
                     }
                 }
 
                 result += smallest;
-                AddedCounts[smallest] ++;
+                AddedCounts[smallest - 'a'] ++;
                 lastIndex = n;
             } while (smallest != c);
         }
         else {
             char smallest = 0;
-            for (auto it : counts) {
-                if (it.second > AddedCounts[it.first]) {
+            for (int i = 0; i < 26; i++) {
+                if (counts[i] > AddedCounts[i]) {
                     // smallest wanted.
-                    smallest = it.first;
+                    smallest = i + 'a';
                     break;
                 }
             }
 
             if (smallest == c) {
                 result += smallest;
-                AddedCounts[smallest] ++;
+                AddedCounts[smallest - 'a'] ++;
                 lastIndex = i;
             }
         }
