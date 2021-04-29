8c937198e1e5101afdb1ef4cd7784b36bda44dd1
polar-zhang <zhyuanshan@gmail.com>
2021-04-28 20:55:30 -0400

    One testcase timeout

---
+++ C++/MinMaxRiddle/main.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the riddle function below.
+vector<long> riddle(vector<long> arr) {
+    // complete this function
+    vector<long> result(arr.size(), 0);
+    int n = 0;
+    long min_value = arr[0], all_min = false;
+    while(n < arr.size()){
+            result[n] = arr[0];
+            for(int i = 1;!all_min && i < arr.size() - n;i ++){
+                if(n == 0){
+                    min_value = min(min_value, arr[i]);
+                }
+                else {
+                    all_min &= min_value == arr[i];
+                }
+                
+                result[n] = max(arr[i], result[n]);
+                arr[i - 1] = min(arr[i - 1], arr[i]);
+        }
+        
+        n ++;
+    }
+    
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<long> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        long arr_item = stol(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<long> res = riddle(arr);
+
+    for (int i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
