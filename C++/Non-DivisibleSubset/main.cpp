f904f6db4dba2ff85411f8a92d10a4f0e5e41dd8
Polar.Zhang <zhyuanshan@gmail.com>
2021-07-13 14:30:56 +0800

    Not a really hard one, but things could be tricky.

---
+++ C++/Non-DivisibleSubset/main.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'nonDivisibleSubset' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY s
+ */
+
+int nonDivisibleSubset(int k, vector<int> s) {
+    vector<int> mods(k, 0);
+    
+    for(int i = 0; i < s.size(); ++i){
+        int mod = s[i] % k;
+        mods[mod] ++;
+    }
+    
+    int count = 0;
+    for(int i = 0; i < k; ++i){
+        if(i == k - i || i == 0){
+             // 1. i == 0: Only one of k's multiple can be added.
+             // 2. i == k - i: Only one of middle-number can be added, because the sum of two of them yields a multiple of k.
+            count += mods[i] > 0 ? 1 : 0; 
+            continue;
+        }
+        
+        int n = max(mods[i], mods[k - i]);// 3. Pick the bigger count of the two sides.
+        count += n;
+        mods[i] = 0; // 4. avoid recalculation.
+        mods[k - i] = 0;
+    }
+    
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string s_temp_temp;
+    getline(cin, s_temp_temp);
+
+    vector<string> s_temp = split(rtrim(s_temp_temp));
+
+    vector<int> s(n);
+
+    for (int i = 0; i < n; i++) {
+        int s_item = stoi(s_temp[i]);
+
+        s[i] = s_item;
+    }
+
+    int result = nonDivisibleSubset(k, s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
