62a63fa49b88d27a017543bae5946fed39aabaf8
Polar.Zhang <zhyuanshan@gmail.com>
2021-10-29 18:17:26 +0800

    Finish coding ...

--- C++/StringFunctionCalculation/main.cpp
+++ C++/StringFunctionCalculation/main.cpp
@@ -54,9 +54,9 @@ vector<Suffix> getSuffixArray(const string& text){
                 suffixArr[i].rank1 = suffixArr[i - 1].rank1;
             }
             else{
-                suffixArr[i].rank1 = suffixArr[i - 1].rank1 + 1;
                 preRank1 = suffixArr[i].rank1;
                 preRank2 = suffixArr[i].rank2;
+                suffixArr[i].rank1 = suffixArr[i - 1].rank1 + 1;
             }
         }
 
@@ -74,19 +74,80 @@ vector<Suffix> getSuffixArray(const string& text){
 
 // https://www.geeksforgeeks.org/%C2%AD%C2%ADkasais-algorithm-for-construction-of-lcp-array-from-suffix-array/
 // kasai’s Algorithm 
-void makeLcp(Suffix suffix){
+// Let lcp of suffix beginning at txt[i] be k. If k is greater than 0, then lcp for suffix beginning at txt[i+1] will be at-least k-1. 
+// The reason is, relative order of characters remain same. If we delete the first character from both suffixes, we know that at least k - 1 characters will match. 
+// For example for substring “ana”, lcp is 3, so for string “na” lcp will be at-least 2. Refer this for proof.
+vector<int> getLcp(const vector<Suffix>& suffixArr, const string& text){
+    int n = suffixArr.size();
+    vector<int> lcp(n);
+    vector<int> selfIndexes(n); // The index position of the suffixes.
+
+    for(int i = 0;i < n; ++i){
+        selfIndexes[suffixArr[i].index] = i;
+    }
+
+    int leastMatch = 0; // ... at least k - 1 characters will match
+    for(int i = 0;i < n; ++i){ // i represents original index. In order to use pre-calculated k, we don't compare strings in suffixArr's order, but instead use original index's order.
+        int next = selfIndexes[i] + 1; // The next suffix index  of the i-th string in suffixArr.
+        if(next >= n) continue;
+
+        int p = leastMatch + i;
+        int q = leastMatch + suffixArr[next].index;
+        while(p < text.size() && q < text.size() && text[p] == text[q]){ // compare from leastMatch.
+            leastMatch ++;
+            p ++;
+            q ++;
+        }
+
+        lcp[selfIndexes[i]] = leastMatch; // Don't forget the i-th string's position is selfIndexes[i].
+        if(leastMatch > 0){
+            leastMatch --; // Next least matches.
+        }
+    }
+
+    lcp[n - 1] = 0;
+    return lcp;
+}
+
+// https://www.hackerrank.com/challenges/string-function-calculation/forum/comments/1024871
+int findLargestRetangleInHistogram(vector<int> lcp){
+    stack<int> s;
     
+    int maxRect = 0;
+    int n = 0;
+    while(n < lcp.size() || !s.empty()){
+        if(s.empty() || n < lcp.size() && lcp[s.top()] <= lcp[n]) {
+            s.push(n);
+            n ++;
+        }
+        else {
+            int top = s.top();
+            s.pop();
+            maxRect = max(maxRect, (lcp[top] + 1) * (s.empty() ? n : n - s.top() - 1));
+        }
+    }
+
+    return maxRect;
 }
 
 // https://www.hackerrank.com/challenges/string-function-calculation/forum/comments/1031872
-// https://www.hackerrank.com/challenges/string-function-calculation/forum/comments/1024871
 int maxValue(string t) {
-    int maxVal = 0;
-    vector<Suffix> suffixArr = getSuffixArray("banana");
+    
+    vector<Suffix> suffixArr = getSuffixArray(t);
     for(auto s : suffixArr){
-        cout << s.index << "\n";
+        cout << s.index << " ";
+    }
+    cout << "\n";
+
+    vector<int> lcp = getLcp(suffixArr, t);
+    for(auto n : lcp){
+        cout << n << " ";
     }
 
+    cout << "\n";
+    int maxVal = 0;
+    maxVal = findLargestRetangleInHistogram(lcp);
+    cout << maxVal;
     return maxVal;
 }
 
