29acefaa996f61e197a1ca648d7a8b0e3a99f1d9
Polar.Zhang <zhyuanshan@gmail.com>
2021-10-29 12:10:23 +0800

    Order sufffix arrays.

--- C++/StringFunctionCalculation/main.cpp
+++ C++/StringFunctionCalculation/main.cpp
@@ -2,84 +2,91 @@
 
 using namespace std;
 
-struct SuffixNode{
-   SuffixNode *children[26];
-   string suffix;
-   int repeatCount;
-   bool leaf;
-   SuffixNode(){
-       repeatCount = 0;
-       leaf = true;
-       memset(children, 0 , sizeof(SuffixNode*) * 26);
-   }
+struct Suffix{
+    int index; // original index.
+    int rank1;
+    int rank2;
 };
 
-// https://www.hackerearth.com/practice/notes/trie-suffix-tree-suffix-array/
-// Put c into Trie.
-// Mark maxVal.
-void insertString(SuffixNode &root, string &t, int start, int &maxVal){
-    int depth = 0;
-    SuffixNode *p = &root;
-    cout << "\n" << t.substr(start, t.size() - start) << ": ";
-
-    for(int i = start; i < t.size(); ++i){
-        int index = t[i] - 'a';
-        depth ++; // Actually a string lenth.
-        
-        SuffixNode *child = p->children[index];
-        if(child == NULL){
-            if(!p->leaf){
-                p->children[index] = new SuffixNode();
-                cout << "next: " << t[i] << ", ";
-            }
-            else{
-                // This is a leaf.
-                if(p->suffix.size() == 0){
-                    p->suffix = t.substr(i, t.size() - i);
-                    p->repeatCount ++;
-                    maxVal = max(maxVal, (int)t.size() - start);
-                    cout << "suffix: " << t[i] << ", ";
-                    break;
-                }
-                else {
-                    // Break down the previous suffix.
-                    int ix = p->suffix[0] - 'a';
-                    p->children[ix] = new SuffixNode();
-                    p->children[ix]->repeatCount = 1;
-                    p->children[ix]->suffix = p->suffix.erase(0, 1);
-                    p->suffix.clear();
-                    p->leaf = false;
-                    cout << "break: " << p->suffix[0] << ", ";
-
-                    if(ix != index){
-                        // Fork a branch.
-                        p->children[index] = new SuffixNode();
-                        p = p->children[index];
-                        p->suffix = t.substr(i, t.size() - i);
-                        p->repeatCount ++;
-                        maxVal = max(maxVal, (int)t.size() - start);
-                        cout << "fork: " << p->suffix[0] << ", ";
-                        break;
-                    }
-                }
-            }
+bool compare(Suffix suffix1, Suffix suffix2) {
+        if(suffix1.rank1 < suffix2.rank1){
+            return true;
+        }
+        else if(suffix1.rank1 > suffix2.rank1) {
+            return false;
         }
         
-        p = p->children[index];
-        p->repeatCount ++;
-        maxVal = max(maxVal, p->repeatCount * depth);
+        return suffix1.rank2 < suffix2.rank2;
     }
+
+// https://www.geeksforgeeks.org/suffix-array-set-1-introduction/
+// https://www.geeksforgeeks.org/suffix-array-set-2-a-nlognlogn-algorithm/
+// Sort suffix strings with O(nLogn) algorithm
+vector<Suffix> getSuffixArray(const string& text){
+    vector<Suffix> suffixArr(text.size());
+
+    auto n = suffixArr.size();
+
+    // Assign original index & rank1.
+    for(int i = 0;i < n; ++i){
+        suffixArr[i].index = i;
+        suffixArr[i].rank1 = text[i] - 'a';
+    }
+
+    // Assign rank2.
+    suffixArr.back().rank2 = -1; // The last one has no next rank.
+    for(int i = 1;i < n; ++i){
+        suffixArr[i - 1].rank2 = suffixArr[i].rank1; // Next rank.
+    }
+
+    sort(suffixArr.begin(), suffixArr.end(), compare); // Sort by the ranks.
+
+    for(int k = 2; k < n; k = 2*k){
+        vector<int> selfIndexes(n); // Index position of suffix.
+        // Re-assign rank1
+        int preRank1 = suffixArr[0].rank1, preRank2 = suffixArr[0].rank2;
+        suffixArr[0].rank1 = 0;
+        selfIndexes[suffixArr[0].index] = 0;
+        for(int i = 1;i < n; ++i){
+            selfIndexes[suffixArr[i].index] = i; // Save the index position of current suffix, to help get the next suffix.
+
+            if(suffixArr[i].rank1 == preRank1 && suffixArr[i].rank2 == preRank2){
+                suffixArr[i].rank1 = suffixArr[i - 1].rank1;
+            }
+            else{
+                suffixArr[i].rank1 = suffixArr[i - 1].rank1 + 1;
+                preRank1 = suffixArr[i].rank1;
+                preRank2 = suffixArr[i].rank2;
+            }
+        }
+
+        // Re-assign rank2.
+        for(int i = 0; i < n; ++i){
+            int nextIndex = suffixArr[i].index + k;
+            suffixArr[i].rank2 = nextIndex < n ? suffixArr[selfIndexes[nextIndex]].rank1 : -1;
+        }
+
+        sort(suffixArr.begin(), suffixArr.end(), compare); // Sort by the ranks.
+    }
+    
+    return suffixArr;
 }
 
+// https://www.geeksforgeeks.org/%C2%AD%C2%ADkasais-algorithm-for-construction-of-lcp-array-from-suffix-array/
+// kasai’s Algorithm 
+void makeLcp(Suffix suffix){
+    
+}
+
+// https://www.hackerrank.com/challenges/string-function-calculation/forum/comments/1031872
+// https://www.hackerrank.com/challenges/string-function-calculation/forum/comments/1024871
 int maxValue(string t) {
     int maxVal = 0;
-    SuffixNode root;
-    
-    for(int i = 0; i < t.size(); ++i){
-        insertString(root, t, i, maxVal);
+    vector<Suffix> suffixArr = getSuffixArray("banana");
+    for(auto s : suffixArr){
+        cout << s.index << "\n";
     }
-    
-    cout << "maxVal: " << maxVal << "\n";
+
     return maxVal;
 }
 
